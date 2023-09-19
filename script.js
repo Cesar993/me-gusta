1d01aa7123bace72788e1dd3e9a385105286dcc2
Cesar993 <ce.serrano@duocuc.cl.com>
2023-09-19 00:40:50 -0300

    Actua

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
 
-var likes = document.querySelector("#cantidadLike")
-var likes2 = document.querySelector("#cantidadLike2")
-var likes3 = document.querySelector("#cantidadLike3")
-var countElement = document.querySelector("btn")
-var countElement = document.querySelector("btn2")
-var countElement = document.querySelector("btn3")
-var count = 9;
-var count2 =12;
-var count3 =9;
-function like() {
+var likes = [9,12,9];
+var person = [
+    document.querySelector("#cantidadLike"),
+    document.querySelector("#cantidadLike2"),
+    document.querySelector("#cantidadLike3")
+]
+function like(num) {
 
-    likes.innerText = count + " like(s)"
-    count ++;
+    likes[num] ++;
+    person[num].innerText = likes[num] + " like(s)";   
 }
-function like2() {
 
-    likes2.innerText = count2 + " like(s)"
-    count2 ++;
-}
-function like3() {
-
-    likes3.innerText = count3 + " like(s)"
-    count3 ++;
-}
 
